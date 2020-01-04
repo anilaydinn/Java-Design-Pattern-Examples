d4b0fa3c857e9e6acf32ad7c9a8ec9fd2a9bd7a6
anilaydinn <anil_aydinn@outlook.com>
2020-01-04 21:15:25 +0300

    CommandPatternTutorialpointExample added.

---
+++ CommandPatternTutorialsPointExample/src/CommandPatternTutorialspointExample.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandPatternTutorialspointExample {
+	
+	public static void main(String[] args) {
+		
+		Stock abcStock = new Stock();
+		
+		BuyStock buyStockOrder = new BuyStock(abcStock);
+		SellStock sellStockOrder = new SellStock(abcStock);
+		
+		Broker broker = new Broker();
+		broker.takeOrder(buyStockOrder);
+		broker.takeOrder(sellStockOrder);
+		
+		broker.placeOrders();
+	}
+}
+
+interface Order{
+	
+	void execute();
+}
+
+class Stock{
+	
+	private String name = "ABC";
+	private int quantity = 10;
+	
+	public void buy() {
+		
+		System.out.println("Stock [ Name: " + name + ", Quantity: " + quantity + " ] bought");
+	}
+	
+	public void sell() {
+		
+		System.out.println("Stock [ Name: " + name + ", Quantity: " + quantity + " ] sold");
+	}
+}
+
+class BuyStock implements Order{
+	
+	private Stock abcstock;
+	
+	public BuyStock(Stock abcStock) {
+		
+		this.abcstock = abcStock;
+	}
+	
+	@Override
+	public void execute() {
+		
+		abcstock.buy();
+	}
+}
+
+class SellStock implements Order{
+	
+	private Stock abcStock;
+	
+	public SellStock(Stock abcStock) {
+		
+		this.abcStock = abcStock;
+	}
+	
+	public void execute() {
+		
+		abcStock.sell();
+	}
+}
+
+class Broker{
+	
+	private List<Order> orderList = new ArrayList<Order>();
+	
+	public void takeOrder(Order order) {
+		
+		orderList.add(order);
+	}
+	
+	public void placeOrders() {
+		
+		for(Order order : orderList) {
+			
+			order.execute();
+		}
+		orderList.clear();
+	}
+}
